be1887a78591f009fba48fa21e1645b966d2984e
Adrian <adrian.talonia11@gmail.com>
2023-09-14 00:15:32 -0600

    add entities

---
+++ kotlin-springboot-gradle/src/main/kotlin/com/tr/ksbg/model/entity/PostEntity.kt
@@ -0,0 +1,18 @@
+package com.tr.ksbg.model.entity
+
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "posts")
+class PostEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    val id: UUID? = null,
+    @Column
+    val title: String,
+    val description: String? = null,
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    val author: UserEntity
+)
---
+++ kotlin-springboot-gradle/src/main/kotlin/com/tr/ksbg/model/entity/UserEntity.kt
@@ -0,0 +1,16 @@
+package com.tr.ksbg.model.entity
+
+import jakarta.persistence.*
+import java.util.UUID
+
+@Entity
+@Table(name = "users")
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    val id: UUID? = null,
+    @Column
+    val name: String,
+    @OneToMany(mappedBy = "author")
+    val posts: Set<PostEntity> = setOf(),
+)
