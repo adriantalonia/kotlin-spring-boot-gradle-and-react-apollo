73417d48309a536defd312a8a3c85e265f65a6ea
Adrian <adrian.talonia11@gmail.com>
2023-11-02 22:23:44 -0600

    add comment mutation

--- kotlin-springboot-gradle/src/main/kotlin/com/tr/ksbg/service/CommentService.kt
+++ kotlin-springboot-gradle/src/main/kotlin/com/tr/ksbg/service/CommentService.kt
@@ -1,13 +1,19 @@
 package com.tr.ksbg.service
 
 import com.tr.ksbg.model.dto.Comment
+import com.tr.ksbg.model.entity.CommentEntity
+import com.tr.ksbg.model.input.AddCommentInput
 import com.tr.ksbg.repository.CommentRepository
+import com.tr.ksbg.repository.PostRepository
+import com.tr.ksbg.repository.UserRepository
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import java.util.UUID
 
 @Service
-class CommentService(private val commentRepository: CommentRepository) {
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val userRepository: UserRepository) {
 
     fun getComments(page: Int, size: Int): List<Comment> {
         return commentRepository.findAll(PageRequest.of(page, size)).map {
@@ -36,5 +42,26 @@ class CommentService(private val commentRepository: CommentRepository) {
                 text = it.text
             )
         }.toList()
+    }
+
+    fun addComment(addCommentInput: AddCommentInput): Comment {
+        val user = userRepository.findById(addCommentInput.authorId)
+            .orElseThrow { RuntimeException("user does not exist with id: ${addCommentInput.authorId}") }
+
+        val post = user.posts.firstOrNull{ it.id == addCommentInput.postId}
+            ?: throw RuntimeException("Post does not exist with id: ${addCommentInput.postId}")
+
+        val comment = CommentEntity(
+            text = addCommentInput.text,
+            author = user,
+            post = post
+        )
+
+        val commentCreated = commentRepository.save(comment)
+
+        return Comment(
+            id = commentCreated.id!!,
+            text = commentCreated.text
+        )
     }
 }
